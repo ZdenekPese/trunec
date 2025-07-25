6ed14ebafe3e608767275efe89d81fc86ee7bf94
Zdeněk Pešek <zzdenek.pesek@gmail.com>
2025-07-25 14:30:44 +0200

    Complete website redesign - Trunec Elektroinstalace business website

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1>Build in progress! Pan Trunec bude mít vlastní stránky, přijď sem za nějakou dobu :)</h1>
-    </main>
-  );
-}
