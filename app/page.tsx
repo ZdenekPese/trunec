bcdbe1a17782ce8a32b74096d7db2982bdc9b7bb
Zdeněk Pešek <zzdenek.pesek@gmail.com>
2025-07-25 12:59:58 +0200

    new text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1>Hello from Next.js on Vercel!</h1>
+      <h1>Build in progress! Pan Trunec bude mít vlastní stránky, přijď sem za nějakou dobu :)</h1>
     </main>
   );
 }
