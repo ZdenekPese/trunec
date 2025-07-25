30651ab9a736523a9ffd3df9545cff567bda54bd
Zdeněk Pešek <zzdenek.pesek@gmail.com>
2025-07-25 16:20:34 +0200

    deploy

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{textAlign: 'center', padding: '80px'}}>
+      <h1 style={{fontSize: '2rem', color: '#eab308'}}>404 - Stránka nenalezena</h1>
+      <p style={{marginTop: '16px'}}>Omlouváme se, ale požadovaná stránka neexistuje.</p>
+    </div>
+  );
+}
