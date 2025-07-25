a16a03165b9fbffb2888c8ada41748f40b81e276
Zdeněk Pešek <zzdenek.pesek@gmail.com>
2025-07-25 12:09:59 +0200

    Add custom 404 page and update metadata - debug 404 issue

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>This is our custom 404 page - if you see this, the app is working but the route doesn&apos;t exist.</p>
+    </main>
+  );
+}
