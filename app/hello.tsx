e00a21d32a7acd37cde06ad63839fade010882ed
Zdeněk Pešek <zzdenek.pesek@gmail.com>
2025-07-25 11:02:23 +0200

    testovaci commit

---
+++ app/hello.tsx
@@ -0,0 +1,7 @@
+export default function HelloPage() {
+  return (
+    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>Hello from Next.js on Vercel!</h1>
+    </main>
+  );
+}
